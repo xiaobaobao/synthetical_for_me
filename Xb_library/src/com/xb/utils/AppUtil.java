b894d9172a4fcb4ea3564a517700c3ee97a3ed3e
rbh <rbh@jfpal.com>
2015-09-14 18:30:44 +0800

    9.24,建立基础包

---
+++ Xb_library/src/com/xb/utils/AppUtil.java
@@ -0,0 +1,54 @@
+package com.xb.utils;
+
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+
+/**
+ * @todo:
+ * @author: xiaobao
+ * @time: 2015-9-14上午9:52:29
+ */
+public class AppUtil {
+	
+	
+	/**
+     * 获取App包 信息版本号
+     * @param context
+     * @return
+     */
+	public static PackageInfo getPackageInfo(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return packageInfo;
+    }
+	
+	
+	/**
+     * need < uses-permission android:name =“android.permission.GET_TASKS” />
+     * 判断是否前台运行
+     */
+    public static boolean isRunningForeground(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> taskList = am.getRunningTasks(1);
+        if (taskList != null && !taskList.isEmpty()) {
+            ComponentName componentName = taskList.get(0).topActivity;
+            if (componentName != null && componentName.getPackageName().equals(context.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+}
